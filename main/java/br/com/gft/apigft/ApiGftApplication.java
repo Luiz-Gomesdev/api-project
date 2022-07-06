65cc321b572b8b8f8dc9f9e88de093e644d38ed3
logm <luiz-augusto.gomes@gft.com>
2022-07-06 16:09:05 -0300

    Iniciando o projeto

---
+++ main/java/br/com/gft/apigft/ApiGftApplication.java
@@ -0,0 +1,13 @@
+package br.com.gft.apigft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGftApplication.class, args);
+	}
+
+}
